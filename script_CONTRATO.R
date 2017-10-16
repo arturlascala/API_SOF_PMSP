ffbf0958725b8f3e72ef9a38751ea87c18589e8a
arturlascala <arturlascala@gmail.com>
2017-10-16 10:31:18 -0200

    Update script_CONTRATO.R

--- script_CONTRATO.R
+++ script_CONTRATO.R
@@ -21,12 +21,15 @@ APISOF_contratos <- function(SENHA, anoContrato = NULL, codOrgao = NULL){
   prov2 <- fromJSON(content(prov1, "text", encoding = "UTF-8"))
   paginas <- prov2$metadados$qtdPaginas
   
+  pb <- txtProgressBar(min = 0, max = paginas, style = 3)
+  
   x <- vector("list", paginas)
   for (i in 1:paginas) {
     
     x[[i]] <- GET(paste0("https://gatewayapi.prodam.sp.gov.br:443/financas/orcamento/sof/v2.1.0/consultaContrato?anoContrato=",anoContrato, "&numPagina=",i, "&codOrgao=", codOrgao),
                   add_headers(Accept = "application/json", 
                               Authorization = paste("Bearer", SENHA)))
+    setTxtProgressBar(pb, i)
   }
   
   # converte o JSON em data.frame
