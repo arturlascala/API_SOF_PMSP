d0b52067f247f6e826eef0fbe5fddd794ddc80ab
arturlascala <arturlascala@gmail.com>
2017-10-16 10:32:13 -0200

    Update script_EMPENHO.R

--- script_EMPENHO.R
+++ script_EMPENHO.R
@@ -21,12 +21,15 @@ APISOF <- function(SENHA, anoEmpenho, mesEmpenho, codEmpenho = NULL, numCpfCnpj 
   prov2 <- fromJSON(content(prov1, "text", encoding = "UTF-8"))
   paginas <- prov2$metadados$qtdPaginas
   
+  pb <- txtProgressBar(min = 0, max = paginas, style = 3)
+  
   x <- vector("list", paginas)
   for (i in 1:paginas) {
     
     x[[i]] <- GET(paste0("https://gatewayapi.prodam.sp.gov.br:443/financas/orcamento/sof/v2.1.0/consultaEmpenhos?anoEmpenho=",anoEmpenho, "&mesEmpenho=", mesEmpenho,"&numCpfCnpj=", numCpfCnpj, "&txtRazaoSocial=", txtRazaoSocial,"&codOrgao=",codOrgao, "&numPagina=",i),
                   add_headers(Accept = "application/json", 
                               Authorization = paste("Bearer", SENHA)))
+    setTxtProgressBar(pb, i)
   }
   
   # converte o JSON em data.frame
